91480913f12e0fd960a97d863ef2bbdb35826bb3
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-02-01 23:50:55 -0300

    init: primeira aula das Imersões em Python.

---
+++ Aula-0.py
@@ -0,0 +1,54 @@
+#jeferson - NameError- not defined;
+
+'cinza'
+'Jeferson'
+'U2'
+
+#Serve para mostrar algo na tela. 
+print('Boas vindas a aula de Python!')
+
+# PRINT('Boas vidas') - Gera um erro  falando que "PRINT" não esta definido. 
+
+#Variaveis 
+name = 'Jeferson Alves'
+
+#cor favorita - invalid syntax
+#Snake-Case
+cor_favorita = 'Azul'
+
+#Mostrando variaveis na tela.  
+print(name, cor_favorita)
+
+#
+saldo = 5000
+
+saldo = saldo + 2000
+
+print(f"Saldo na sua conta: R${saldo}")
+
+saldo = saldo - 500
+print(f"Saldo na sua conta: R${saldo}")
+
+saldo = saldo * 2
+print(f"Saldo na sua conta: R${saldo}")
+
+#Import
+import this
+
+#Deixar claro o nome da variável. 
+tenis_nike = 250
+
+idade = 16
+
+#Se a for maior que 18
+if idade > 18: 
+    #pode dirigir
+    print('Pode dirigir')
+#Se não    
+else:
+    #não pode dirigir
+    print('Não pode dirigir')
+
+
+
+
